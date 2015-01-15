90618d03863ac9caf070de85a028ec64b186e57b
Bas van Driel <basvandriel94@gmail.com>
2015-01-15 20:03:39 +0100

    update

---
+++ src/Bas/RoadTaxDataParser/Formatter/formatters/BusFormatter.php
@@ -0,0 +1,12 @@
+<?php
+
+    namespace Bas\RoadTaxDataParser\Formatter\Formatters;
+
+    use Bas\RoadTaxDataParser\Formatter\Formatter;
+
+    class BusFormatter implements Formatter
+    {
+        public function format() {
+            //...
+        }
+    }
